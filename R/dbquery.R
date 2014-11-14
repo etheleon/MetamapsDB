1ce7d04f8e9df64eb6d303a2fa553ebd342338bc
etheleon <picy2k@gmail.com>
2014-11-14 16:12:06 +0800

    dbquery.R

---
+++ R/dbquery.R
@@ -0,0 +1,24 @@
+dbquery<-structure(function(#Function for querying metamaps DB
+###Takes variable queries with table output ie. no nodes no rels
+query,	##<< Cypher Query, rmbr to escape double quotes if any 
+params = FALSE, ##<< a list object. If no parameters, let params = False.
+cypherurl ## The address of the graph database eg. metamaps.scelse.nus.edu.sg:7474
+){
+#Checks if the params is given
+if(is.list(params)){
+        post = toJSON(list(query = query, params = params))
+}else{
+        post = toJSON(list(query = query))
+}
+
+result = fromJSON(getURL(cypherurl, customrequest = "POST", httpheader = c(`Content-Type` = "application/json"), postfields = post))
+inbetween = as.data.frame(
+    do.call(rbind,lapply(result$data, function(x) matrix(x, nrow=1) )))
+    as.data.frame(t(apply(inbetween,1,unlist))) #problematic df structure
+}, ex=function(x){ 
+    output.df <- dbquery(
+     query = "START ko=node:koid('ko:\"ko:K00020\"') return ko.ko,ko.definition",
+     params = FALSE, 
+     cypherurl = "metamaps.scelse.nus.edu.sg:7474/db/data/cypher")
+    #cypherurl = "192.168.100.1:7474/db/data/cypher"    #internal within the server
+})
