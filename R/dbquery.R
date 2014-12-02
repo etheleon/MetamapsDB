7055bbdb0a172011da3d553ef93153714e8cf54e
etheleon <picy2k@gmail.com>
2014-12-02 11:13:11 +0800

    dbquery deals with out empty columns

--- R/dbquery.R
+++ R/dbquery.R
@@ -2,7 +2,7 @@ dbquery<-structure(function(#Function for querying metamaps DB
 ###Takes variable queries with table output ie. no nodes no rels
 query,	##<< Cypher Query, rmbr to escape double quotes if any 
 params = FALSE, ##<< a list object. If no parameters, let params = False.
-cypherurl = "metamaps.scelse.nus.edu.sg:7474/db/data/cypher", ## The address of the graph database eg. metamaps.scelse.nus.edu.sg:7474
+cypherurl = "192.168.100.1:7474/db/data/cypher", ## The address of the graph database eg. metamaps.scelse.nus.edu.sg:7474
 ...
 ){
 #Checks if the params is given
@@ -13,14 +13,17 @@ if(is.list(params)){
 }
 
 result = fromJSON(getURL(cypherurl, customrequest = "POST", httpheader = c(`Content-Type` = "application/json"), postfields = post))
-
-setNames(
-    data.frame(lapply(result$data[[1]], function(x) { matrix(x, ncol=1) })), 
-    result$columns)
+if(length(result$data)>1){
+setNames(data.frame(do.call(rbind,lapply(result$data, function(x) matrix(x, nrow=1)))), result$columns)
+}else{
+setNames(data.frame(lapply(result$data[[1]], function(x){
+if(length(x) == 0){matrix(NA, nrow=1)}else{matrix(x, nrow=1)}
+})), result$columns)
+}
 }, ex=function(x){ 
     output.df <- dbquery(
      query = "START ko=node:koid('ko:\"ko:K00020\"') return ko.ko,ko.definition",
      params = FALSE, 
-     cypherurl = "metamaps.scelse.nus.edu.sg:7474/db/data/cypher")
-    #cypherurl = "192.168.100.1:7474/db/data/cypher"    #internal within the server
+    #cypherurl = "metamaps.scelse.nus.edu.sg:7474/db/data/cypher")
+    cypherurl = "192.168.100.1:7474/db/data/cypher")    #internal within the server
 })
