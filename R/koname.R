9ba7bd36f4f02aac7af95cbd66eac4b4fec400fb
etheleon <picy2k@gmail.com>
2015-01-12 17:16:05 +0800

    koname has minimal option

--- R/koname.R
+++ R/koname.R
@@ -1,12 +1,20 @@
 koname<-structure(function #List Pathways
-    (ko='K00001', ##<< The default is K00001, accepts both with or without the ko: prefix
+    (ko='K00001', 
+     minimal=TRUE,##<< The default is K00001, accepts both with or without the ko: prefix
     ... ){
     if(!grepl("ko:",ko)) ko = gsub("^","ko:",ko)
     params=list(koname=ko)
+    if(minimal){
+        query = "START 
+        ko=node:koid(ko={koname}) 
+        RETURN 
+        ko.name, ko.definition"
+    }else{
     query = "START 
         ko=node:koid(ko={koname}) 
     RETURN 
         ko.name, ko.definition, ko.pathway, ko.`pathway.name`"
+    }
     dbquery(query=query, params=params)
     }, ex= function(){
    pathwayDF = koname(ko="K00001");
