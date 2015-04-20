a35701b79e8a59a16fa794a9bbbfb17db0ff0b6e
etheleon <picy2k@gmail.com>
2015-04-20 10:58:57 +0800

    new ig2ggvis function

--- R/ig2ggvis.R
+++ R/ig2ggvis.R
@@ -5,6 +5,7 @@ ig2ggvis<-structure(function #Convert igraph to ggvis object
         layoutDF    = setNames(data.frame(layout.norm(g$layout, xmax=1, xmin=0, ymin=0, ymax=1)), c("x", "y"))
         vertexDF    = data.frame(id = V(g)$name, name = V(g)$label)
         edgeDF      = get.edgelist(g)
+        edgeDF = edgeDF[!duplicated(do.call(c,lapply(1:nrow(edgeDF), function(x) paste(sort(edgeDF[x,]), collapse="")))),]
         vertexDF2   = do.call(rbind,lapply(1:nrow(edgeDF), function(row){
                      v1 = edgeDF[row,][[1]]
                      v2 = edgeDF[row,][[2]]
@@ -23,14 +24,8 @@ ig2ggvis<-structure(function #Convert igraph to ggvis object
         }))
         #graphDF = cbind(layoutDF, vertexDF)
         vertexDF2$id = 1:nrow(vertexDF2)
-        vertexDF2 %>% 
-            ggvis(~x, ~y, key:= ~id)                                                                                                                                                %>%
-            layer_points()                                                                                                                                                          %>%
-            group_by(row)                                                                                                                                                           %>%
-            layer_paths()                                                                                                                                                           %>%
-            add_axis("x", title = "", properties = axis_props(axis = list(strokeWidth = 0), grid = list(strokeWidth = 0),ticks = list(strokeWidth = 0), labels = list(fontSize=0))) %>%
-            add_axis("y", title = "", properties = axis_props(axis = list(strokeWidth = 0), grid = list(strokeWidth = 0),ticks = list(strokeWidth = 0), labels = list(fontSize=0))) %>%
-            add_tooltip(function(data){paste0("ID: ", data$name, "<br>", "label: ", vertexDF2[vertexDF2$id == data$id, ]$label   )})
+        vertexDF2$type = do.call(c,lapply(strsplit(x=as.character(vertexDF2$name), split=":"), function(x) x[[1]]))
+        vertexDF2
     }, ex= function(){
         ...
     })
