3292c5cd9ee71d828f530fea6f5388b3f13668cb
etheleon <picy2k@gmail.com>
2015-05-27 11:32:25 +0800

    new functions: igraph2gexf and findSeeds
    igraph2gexf cause the one in regxf isnt working
    (still can be prototyped further, havent tested fully with sigma.js)
    
    findSeeds is still being prototyped

---
+++ R/igraph2gexf.R
@@ -0,0 +1,23 @@
+igraph2gexf <- structure( ##Function for converting igraph 2 gexf obj
+function(mbgraph,  #igraph Obj
+         opacity=1 #Opacity of the nodes
+         ){
+write.gexf(
+      nodes =   tmpnodes %>% select(name, Definition),
+      edges =   tmpedges %>% select(from:to),
+      nodesVizAtt = list(
+              color     =   cbind(
+                                V(mbgraph)$color %>% col2rgb %>% t %>% data.frame,
+                                opacity) %>%
+                            setNames(c("R", "G", "B", "A")), #%>% head
+
+              position  =   cbind(
+                                data.frame(mbgraph$layout), 
+                                opacity) %>%
+                            setNames(c("X","Y","Z")) #%>% head
+               )
+    )
+}, ex=function(){
+...
+})
+
