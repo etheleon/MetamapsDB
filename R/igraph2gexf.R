9f7a2cda9928f8d271db3a880f229b8ea019c646
etheleon <picy2k@gmail.com>
2015-05-28 10:38:00 +0800

    update igraph2gexf

--- R/igraph2gexf.R
+++ R/igraph2gexf.R
@@ -2,12 +2,12 @@ igraph2gexf <- structure( ##Function for converting igraph 2 gexf obj
 function(mbgraph,  #igraph Obj
          opacity=1 #Opacity of the nodes
          ){
+    gdata     = igraph::get.data.frame(mbgraph, what="both")
+      tmpedges  = gdata$edges
+      tmpnodes  = gdata$vertices
 write.gexf(
-      gdata = get.data.frame(mbgraph, what="both")
-      tmpedges <- gdata$edges
-      tmpnodes <- gdata$vertices
-      nodes =   tmpnodes %>% select(name, Definition),
-      edges =   tmpedges %>% select(from:to),
+            nodes     = tmpnodes %>% select(name, Definition),
+      edges     = tmpedges %>% select(from:to),
       nodesVizAtt = list(
               color     =   cbind(
                                 V(mbgraph)$color %>% col2rgb %>% t %>% data.frame,
