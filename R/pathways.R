6232895dddf68de6acee0a54d868cfbb8f69da32
etheleon <picy2k@gmail.com>
2015-06-09 15:48:28 +0800

    Update pathways.R

--- R/pathways.R
+++ R/pathways.R
@@ -1,6 +1,6 @@
 pathways<-structure(function #List Pathways
     (...){
-   setNames(dbquery(query='match (pathway:pathway) return pathway.pathwayname, pathway.pathway'), c("pathwayName", "pathwayID"))
+   setNames(dbquery(query='match (pathway:pathway) return pathway.pathwayname, pathway.pathway'), c("pathwayName", "pathwayID"), ...)
     }, ex= function(){
    pathwayDF = pathways();
    })
