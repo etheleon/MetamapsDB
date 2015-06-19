ac0198e988fbf374ebd4fc18ecad82ee7e7f9ede
etheleon <picy2k@gmail.com>
2015-06-20 00:51:59 +0800

    fixed igraph2gexf; opacity

--- R/igraph2gexf.R
+++ R/igraph2gexf.R
@@ -1,20 +1,20 @@
 igraph2gexf <- structure( 
-function(mbgraph  #Function for converting igraph 2 gexf obj
+function(mbgraph  ##<<Function for converting igraph 2 gexf obj
 ### Converts a igraph object into gexf format
-         ){
+){
     gdata     = igraph::get.data.frame(mbgraph, what="both")
     tmpedges  = gdata$edges
     tmpnodes  = gdata$vertices
     tmpnodes$Definition %<>% sapply(function(x) gsub("[[:punct:]]", " ", x))
     write.gexf(
             nodes     = tmpnodes %>% select(name, Definition),
-      edges     = tmpedges %>% select(from:to),
+            edges     = tmpedges %>% select(from:to),
       nodesVizAtt = list(
               color     =   cbind(
                                 V(mbgraph)$color %>% col2rgb %>% t %>% data.frame,
-                                V(mbgraph)$opacity) %>%
+                                ifelse(is.null(V(mbgraph)$opacity), "100", V(mbgraph)$opacity)
+                                ) %>%
                             setNames(c("R", "G", "B", "A")), #%>% head
-
               position  =   cbind(
                                 data.frame(mbgraph$layout), 
                                 0) %>%
