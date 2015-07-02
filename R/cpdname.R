32b8a29c5d39c9cd74e6d215dff2552a7d0bbb72
etheleon <picy2k@gmail.com>
2015-07-02 10:38:20 +0800

    cpdname now accepts more than one cpd

--- R/cpdname.R
+++ R/cpdname.R
@@ -1,14 +1,19 @@
 cpdname<-structure(function #List Pathways
 ### Finds the details of the CPD when given its ID
-    (cpd='C00022',##<< the cpd ID
+    (cpd='C00022',##<< the cpd ID; either as a single or vector accepts with or without prefix
      minimal=TRUE,##<< The default is K00001, accepts both with or without the ko: prefix
     ... ){
     if(!grepl("cpd:",cpd)) cpd = gsub("^","cpd:",cpd)
-    params=list(cpdname=cpd)
-        query = "START
-        cpd=node:cpdid(cpd={cpdname}) 
-        RETURN 
-        cpd.cpd, cpd.name"
+
+    #params=list(cpdname=cpd)
+    params = cpd %>% lapply(function(x) list(cpdname=x)) %>% list(cpds=.)
+    query = "
+    UNWIND
+        { cpds } AS eachcpd
+    OPTIONAL MATCH
+        (thecpd:cpd {cpd : eachcpd.cpdname})
+    RETURN
+        thecpd.cpd, thecpd.name"
     dbquery(query=query, params=params, ...)
     }, ex= function(){
    #cpdname(cpd="C00022");
