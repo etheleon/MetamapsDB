fa53d2891c3489bc1ebdf45fd8e19ea397d88d50
etheleon <picy2k@gmail.com>
2015-07-08 11:15:13 +0800

    cpdname now batch too

--- R/cpdname.R
+++ R/cpdname.R
@@ -1,10 +1,10 @@
 cpdname<-structure(function #List Pathways
 ### Finds the details of the CPD when given its ID
     (cpd='C00022',##<< the cpd ID; either as a single or vector accepts with or without prefix
-     minimal=TRUE,##<< The default is K00001, accepts both with or without the ko: prefix
-    ... ){
-    if(!grepl("cpd:",cpd)) cpd = gsub("^","cpd:",cpd)
-
+     minimal=TRUE,##<< The default is C00002, accepts both with or without the ko: prefix
+    ... 
+    ){
+    cpd = gsub("^(cpd:)*","cpd:",cpd)
     #params=list(cpdname=cpd)
     params = cpd %>% lapply(function(x) list(cpdname=x)) %>% list(cpds=.)
     query = "
@@ -14,7 +14,8 @@ cpdname<-structure(function #List Pathways
         (thecpd:cpd {cpd : eachcpd.cpdname})
     RETURN
         thecpd.cpd, thecpd.name"
-    dbquery(query=query, params=params, ...)
+    df = dbquery(query=query, params=params, ...)
+    df
     }, ex= function(){
    #cpdname(cpd="C00022");
    })
