4c7c51ce62441008d0b592044e8d6f35875a3ecf
etheleon <picy2k@gmail.com>
2015-07-08 10:38:13 +0800

    not working

--- R/koname.R
+++ R/koname.R
@@ -1,22 +1,26 @@
 koname<-structure(function #List Pathways
-### Gives KO details when supplied with KO id                  
+### Gives KO details when supplied with KO id
     (ko='K00001', ##<< the koid
      minimal=TRUE,##<< The default is K00001, accepts both with or without the ko: prefix
-    ... ){
-    if(!grepl("ko:",ko)) ko = gsub("^","ko:",ko)
-    params=list(koname=ko)
-    if(minimal){
-        query = "START 
-        ko=node:koid(ko={koname}) 
+    ...
+    ){
+        ko = gsub("^(ko:)*","ko:",ko)
+        query = "
+        UNWIND
+            { koname } AS KOSS
+        MATCH
+            (ko:ko {ko : koss.ko})
         RETURN 
-        ko.ko, ko.name, ko.definition"
-    }else{
-    query = "START 
-        ko=node:koid(ko={koname}) 
-    RETURN 
-        ko.ko, ko.name, ko.definition, ko.pathway, ko.`pathway.name`"
-    }
-    dbquery(query=query, params=params, ...)
+            ko.ko, ko.name, ko.definition, ko.pathway, ko.`pathway.name`
+        "
+        params <- ko %>% lapply(function(x){list(ko=x)}) %>% list(koname=.)
+        df     <- dbquery(query=query, params=params, ...)
+
+        if(minimal){
+            df %>% select(ko.ko:ko.definition)
+        }else{
+            df
+        }
     }, ex= function(){
    #pathwayDF = koname(ko="K00001");
    })
