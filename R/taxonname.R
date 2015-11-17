cb265bc84de12c951ef9f63007470a34d173b5eb
etheleon <picy2k@gmail.com>
2015-11-18 00:57:57 +0800

    taxonname use integer not char

--- R/taxonname.R
+++ R/taxonname.R
@@ -1,11 +1,11 @@
 #' Lists the taxonomic id's details
 #'
 #' @param taxon The default is Aspergillus oryzae, accepts both name as well as
-#' @param name  to show the name
+#' @param name  search by name
 #' @param ... additional parameters for dbquery
 #' @export
 taxname<-function(taxon=5062,name=FALSE,... ){
-    params=list(taxonname=as.character(taxon))
+    params=list(taxonname=taxon)
     if(name)
     {
         query = "MATCH (taxa:Taxon {name:{taxonName}}) RETURN taxa.taxid AS taxID, taxa.name AS name, labels(taxa) as rank"
