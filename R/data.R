cf694f918b34b7f7f1081c79dbdade1030e51e44
etheleon <picy2k@gmail.com>
2017-03-21 18:07:39 +0800

    Add data description

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Top500kos
+#'
+#' Dataset showing the top 500 KEGG Orthologs (KOS)
+#'
+#' @format A vector of 500 KOs based on KOs ranked by abundance, not sure if the vector is ranked.
+#'
+#'
+'top500kos'
+
+#' nitrogenMetab
+#'
+#' Igraph object containing KOs and compounds involved in nitrogen metabolism
+#'
+#' @format 83 nodes and 251 edges. 50 KOs and 33 compounds
+#'
+'nitrogenMetab'
