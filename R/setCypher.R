88a46315c016667c850e717a7bcbedf9a456a7a5
etheleon <picy2k@gmail.com>
2017-08-17 14:31:45 +0800

    update cypher

--- R/setCypher.R
+++ R/setCypher.R
@@ -11,6 +11,10 @@
 #'
 #' @importFrom dplyr pull
 #' @importFrom magrittr %>%
+#' @importFrom RCurl getURL
+#' @importFrom RJSONIO fromJSON
+#' @importFrom base64enc base64encode
+#' @importFrom tidyverse map bind_rows
 #' @export
 connect <- function(
 url = "192.168.100.1", ##<< URL hosting the neo4j database
@@ -27,5 +31,50 @@ test = TRUE
         message("Sending test query: Searching for K00001")
         connected = suppressMessages(koname('K00001')) %>% pull(ko.name) == 'E1.1.1.1, adh'
         ifelse(connected, "Connection Successful ✅", "Connection Unsuccessful ❌") %>% message
+        #Check for indices
+        if(connected){
+            key         = base64encode(charToRaw(paste(user, password, sep=":")))
+            indexURL    = paste0(url, ":", port, "/db/data/schema/index/")
+            header      = c('Content-Type' = 'application/json', 'Authorization' = paste('Basic', key))
+            indices     = getURL(indexURL, customrequest = "GET", httpheader = header)
+            if(indices == ""){
+                warning("No indices found; Database is not indexed")
+                yn = readline(prompt="Do you want to index now?")
+                if(yn == 'y'){
+                    mapply(Metamaps::index, label = c("ko", "contigs", "contigs", "Taxon", "cpd"), property = c("ko", "contig", "bin", "taxid", "cpd"))
+                }if else (yn == 'n'){
+                    message("OK its your choice, the queries will be slower")
+                }else{
+                    message("I do not recognise the command")
+                }
+            }else{
+                message("Found these indices:")
+                fromJSON(indices) %>% map(as.data.frame) %>% bind_rows
+            }
+        }
     }
+}
+
+
+#' Indexes the database for faster retrieval
+#'
+#' indexes nodes based on the property
+#'
+#' @export
+#' @example
+#' \dontrun{
+#' f = future({
+#'     dbquery(query="CREATE INDEX ON :contigs(contig)", justPost = TRUE)
+#'     dbquery(query="CREATE INDEX ON :contigs(bin)", justPost = TRUE)
+#'     dbquery(query="CREATE INDEX ON :Taxon(taxid)", justPost = TRUE)
+#'     dbquery(query="CREATE INDEX ON :cpd(cpd)", justPost = TRUE)
+#'     message
+#' }) %plan% multiprocess
+#' }
+index <- function(label, property){
+    post = RJSONIO::toJSON(list(property_keys= "contig"))
+    key         = base64encode(charToRaw(paste(cacheEnv$user, cacheEnv$password, sep=":")))
+    header      = c('Content-Type' = 'application/json', 'Authorization' = paste('Basic', key))
+    addindex = paste(gsub("cypher", "schema/index", cacheEnv$cypher), label, sep="/")
+    getURL(addindex, customrequest = "POST", httpheader = header, postfields = post) %>% fromJSON
 }
